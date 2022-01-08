f465801d73ef34f1faf907039f2226fc716c7509
Rudy Casilla <rpccode@gmail.com>
2022-01-07 20:22:33 -0500

    Agregando Nuevos Clientes en la BD

--- js/nuevocliente.js
+++ js/nuevocliente.js
@@ -26,9 +26,7 @@
         e.preventDefault();
 
 
-        const alerta = document.querySelector('.alerta');
-
-        if (alerta) {
+        
                     //leer todos los inputs
                 const nombre =  document.querySelector('#nombre').value;
                 const email =  document.querySelector('#email').value;
@@ -39,33 +37,70 @@
                         imprimirAlerta('Todos los campos son Obligatorios','error');
                         return;
                 }
-                    imprimirAlerta('correcto');
+                    
+                //crear un objeto con la informacion
+                const cliente = {
+                        nombre,
+                        email,
+                        telefono,
+                        empresa,
+                        id: Date.now()
+                }
+
+                    crearNuevoCliente(cliente);
                 
                 }
+                function imprimirAlerta(mensaje, tipo){
+           
+                    //crear alerta 
+                    const divMensaje = document.createElement('div');
+                    divMensaje.classList.add('alert','px-4','py-3','rounded','max-w-lg','mx-auto','mt-6','text-center','border');
+
+                    if (tipo === 'error') {
+                        divMensaje.classList.add('bg-red-100','border-red-400','text-red-700');
+                    }else{
+                        divMensaje.classList.add('bg-green-100','border-green-400','text-green-700');
+
+                    }
+
+                    divMensaje.textContent= mensaje;
+
+                    formulario.appendChild(divMensaje);
+
+                    setTimeout(() => {
+                        divMensaje.remove();
+                    }, 3000);
+
+               
+    }
+
+                function crearNuevoCliente(cliente) {
+                        const transaction = DB.transaction(['crm'],'readwrite');
+
+                        const objectStore  = transaction.objectStore('crm');
+
+                        objectStore.add(cliente);
+
+                        transaction.onerror = function () {
+                            console.log('Hubo un error');
+
+                            imprimirAlerta('Hubo un  error', 'error')
+                        }
+
+                        transaction.oncomplete = function () {
+                            console.log('Cliente Agregado');
+
+                            imprimirAlerta('El Cliente se Agrego Correctamente')
+
+                            setTimeout(() => {
+                                    window.location.href = 'index.html';
+                            }, 3000);
+                        }
+                }
        
-    }
+    
 
-    function imprimirAlerta(mensaje, tipo){
-        //crear alerta 
-        const divMensaje = document.createElement('div');
-        divMensaje.classList.add('alert','px-4','py-3','rounded','max-w-lg','mx-auto','mt-6','text-center','border');
-
-        if (tipo === 'error') {
-            divMensaje.classList.add('bg-red-100','border-red-400','text-red-700');
-        }else{
-            divMensaje.classList.add('bg-green-100','border-green-400','text-green-700');
-
-        }
-
-        divMensaje.textContent= mensaje;
-
-        formulario.appendChild(divMensaje);
-
-        setTimeout(() => {
-            divMensaje.remove();
-        }, 3000);
-
-    }
+    
 
 
 })();
